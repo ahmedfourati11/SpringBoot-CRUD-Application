6313fabde5d10870a3f2a25e1775227b43e305a4
Ahmed Fourati <151881040+ahmedfourati11@users.noreply.github.com>
2025-09-20 20:36:15 +0200

    CrudApplication.java

--- src/main/java/com/GAF/CRUD/CrudApplication.java
+++ src/main/java/com/GAF/CRUD/CrudApplication.java
@@ -1,13 +1,7 @@
 package com.GAF.CRUD;
 
-import com.GAF.CRUD.student.Student;
-import com.GAF.CRUD.student.StudentController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
 public class CrudApplication {
